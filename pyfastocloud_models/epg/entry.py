fce0c61a2054a18fb66e0118c6716fbbdbfdeda1
topilski <atopilski@gmail.com>
2020-01-03 09:28:59 -0500

    Review

--- pyfastocloud_models/epg/entry.py
+++ pyfastocloud_models/epg/entry.py
@@ -7,5 +7,5 @@ class Epg(Document):
     ID_FIELD = 'id'
     URI_FIELD = 'uri'
 
-    meta = {'allow_inheritance': True, 'collection': 'epg', 'auto_create_index': False}
+    meta = {'allow_inheritance': False, 'collection': 'epg', 'auto_create_index': False}
     uri = StringField(default='http://0.0.0.0/epg.xml', max_length=constants.MAX_URL_LENGTH, required=True)
